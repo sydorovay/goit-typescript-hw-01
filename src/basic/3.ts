d1265ea1d777fb74be070306233726e3ca3f34a1
sydorovay <sydorovay@gmail.com>
2024-10-13 22:08:19 +0200

    add basic & generics

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+let value: string | number; // Union type
+let status: 'enable' | 'disable'; // Literal type
---
+++ src/generics/2.ts
@@ -0,0 +1,15 @@
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+}
+
+function compare<T extends AllType>(top: T, bottom: T): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
